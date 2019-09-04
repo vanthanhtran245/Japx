681d3a0a61c59fbc73e13190efe6f1f486e89182
Thanh Tran Van <thanhtran.hce@gmail.com>
2019-09-04 09:19:00 +0700

    Commit

---
+++ Japx/Japx/Japx.h
@@ -0,0 +1,19 @@
+//
+//  Japx.h
+//  Japx
+//
+//  Created by eru-thanhtv on 9/4/19.
+//  Copyright © 2019 eru-thanhtv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Japx.
+FOUNDATION_EXPORT double JapxVersionNumber;
+
+//! Project version string for Japx.
+FOUNDATION_EXPORT const unsigned char JapxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Japx/PublicHeader.h>
+
+
